9ca47258c54d82db71e71337b8bc2b2286e44c0b
Azonkeu <mazonkeu@gmail.com>
2021-12-15 02:31:37 +0100

    Create a to-do-list

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+import {
+  showStorage, checked, storeList, addList,
+} from './app.js';
+/* eslint-disable no-unused-expressions */
+addList;
+storeList;
+checked;
+showStorage;
